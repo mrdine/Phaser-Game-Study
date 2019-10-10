8ea86b56921827b08855df11ec10b3157a0efcbe
Daniel <daniel.gomes071@gmai.com>
2019-10-09 21:11:00 -0300

    melhorado exemplo do jogo de mover as caixas

--- box/src/main.js
+++ box/src/main.js
@@ -16,7 +16,7 @@ function preload() {
     this.load.image('greenCrate', './assets/world/crate_05.png')
     this.load.image('ground', './assets/world/ground_06.png')
     // player
-    this.load.spritesheet('player', './assets/player/sheetPlayer.png', { frameWidth: 55, frameHeight: 64 })
+    this.load.spritesheet('player', './assets/player/sheetPlayer3.png', { frameWidth: 64, frameHeight: 64 })
 }
 
 function create() {
@@ -114,26 +114,49 @@ function create() {
                     break;
             }
             if (value != i) {
-
-                if (value === 7 || value === 8 || value === 9 || value === 10) {
+                if (value === 6) {
                     // bottonLayer
-                    var bl = pbLayer.create(lin * spriteSize, col * spriteSize, theSprite).setOrigin(0, 0)
+                    var coin = groundLayer.create(lin * spriteSize, col * spriteSize, theSprite).setOrigin(0, 0)
+                    // resize sprite
+                    coin.displayWidth = spriteSize
+                    coin.scaleY = 1.25
+                }
+                else if (value === 7 || value === 8 || value === 9 || value === 10) {
+                    // bottonLayer
+                    var bl = bottonsLayer.create(lin * spriteSize, col * spriteSize, theSprite).setOrigin(0, 0)
                     // resize sprite
                     bl.displayWidth = spriteSize
                     bl.scaleY = 1.25
                 } else {
-                    if ((lin === 0 || lin === 9) || col === 0 || col === 9) {
+                    //if ((lin === 0 || lin === 9) || col === 0 || col === 9) 
+                    if (value === 0 || value === 1) {
                         // border layer
                         var border = bordersLayer.create(lin * spriteSize, col * spriteSize, theSprite).setOrigin(0, 0)
                         // resize sprite
                         border.displayWidth = spriteSize
                         border.scaleY = 1.25
+                        border.refreshBody()
                     } else {
                         // pbLayer
+                        //2,3,4,5
                         var pb = pbLayer.create(lin * spriteSize, col * spriteSize, theSprite).setOrigin(0, 0)
                         // resize sprite
                         pb.displayWidth = spriteSize
                         pb.scaleY = 1.25
+                        pb.refreshBody()
+                        // save the crates
+                        if (value === 2) {
+                            gameState.redCrate = pb
+                        } else if (value === 3) {
+                            gameState.woodCrate = pb
+                        }
+                        else if (value === 4) {
+                            gameState.blueCrate = pb
+                        }
+                        else if (value === 5) {
+                            gameState.greenCrate = pb
+                        }
+
                     }
                 }
 
@@ -153,59 +176,79 @@ function create() {
 
     this.anims.create({
         key: 'idleDown',
-        frames: this.anims.generateFrameNumbers('player', { start: 1, end: 1 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 11, end: 11 }),
         frameRate: 1,
         repeat: -1
     });
     this.anims.create({
         key: 'idleUp',
-        frames: this.anims.generateFrameNumbers('player', { start: 3, end: 3 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 0 }),
         frameRate: 1,
         repeat: -1
     });
     this.anims.create({
         key: 'idleLeft',
-        frames: this.anims.generateFrameNumbers('player', { start: 9, end: 9 }),
-        frameRate: 1,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'idleRigth',
         frames: this.anims.generateFrameNumbers('player', { start: 6, end: 6 }),
         frameRate: 1,
         repeat: -1
     });
     this.anims.create({
+        key: 'idleRigth',
+        frames: this.anims.generateFrameNumbers('player', { start: 3, end: 3 }),
+        frameRate: 1,
+        repeat: -1
+    });
+    this.anims.create({
         key: 'runDown',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 9, end: 11 }),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
         key: 'runUp',
-        frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
         key: 'runRigth',
-        frames: this.anims.generateFrameNumbers('player', { start: 6, end: 8 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
         key: 'runLeft',
-        frames: this.anims.generateFrameNumbers('player', { start: 9, end: 11 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 6, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
 
+
     // dfine collision between player and crates
-    this.physics.add.collider(player, pbLayer);
+    this.physics.add.collider(player, pbLayer, moveCrate);
     // dfine collision between player and borders
     this.physics.add.collider(player, bordersLayer);
 
+
+
 }
+
+// move crate when player collides with him
+function moveCrate(player, crate) {
+    var crateSpeed = 100
+    /*
+    switch (gameState.playerDirection) {
+        case 'down':
+            crate.setVelocityY(crateSpeed)
+        case 'up':
+            crate.setVelocityY(-crateSpeed)
+        case 'rigth':
+            crate.setVelocityX(crateSpeed)
+        case 'left':
+            crate.setVelocityX(-crateSpeed)
+    }*/
+}
+
 
 function update() {
 
